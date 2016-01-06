814acc54f36637caf718a69989176a8e1c89443e
Philip Schwarz <pschwarz@expedia.com>
2016-01-06 18:02:41 +0000

    initial

---
+++ src/fpinscala/datastructures/inbuiltlist/InbuiltList.scala
@@ -0,0 +1,20 @@
+package fpinscala.datastructures.inbuiltlist
+
+object InbuiltList {
+
+  assert(           List() ==       Nil, "the empty list is nil")
+  assert(     List(3).head ==         3, "the head of a single-element list it the element")
+  assert(     List(3).tail ==       Nil, "the tail of a single-element list is Nil")
+  assert( List(1,2,3).head ==         1, "the head of an n-element list it the 1st element")
+  assert( List(1,2,3).tail == List(2,3), "the tail of an n-element list is the remaining n-1 elements")
+
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case x :: xs => x + sum(xs)
+  }
+
+  assert( sum(                 Nil ) ==  0, "sum of empty list should be 0")
+  assert( sum(             List(3) ) ==  3, "sum of single-element list should be the element" )
+  assert( sum( List(1, 2, 3, 4, 5) ) == 15, "sum of list should be sum of its elements")
+
+}
