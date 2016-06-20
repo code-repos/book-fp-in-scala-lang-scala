7bafa79cf7c5d9fa3c0602b3d3769eda9a3a7da9
Philip Schwarz <pschwarz@expedia.com>
2016-06-20 22:28:37 +0100

    added assertions creating in-built lists using ::

--- src/fpinscala/datastructures/inbuiltlist/InbuiltList.scala
+++ src/fpinscala/datastructures/inbuiltlist/InbuiltList.scala
@@ -2,6 +2,13 @@ package fpinscala.datastructures.inbuiltlist
 
 object InbuiltList {
 
+  assert(              List() ==      Nil,  "the empty list is nil")
+  assert(       (3::Nil).head ==        3,  "the head of a single-element list it the element")
+  assert(       (3::Nil).tail ==      Nil,  "the tail of a single-element list is Nil")
+  assert( (1::2::3::Nil).head ==        1,  "the head of an n-element list it the 1st element")
+  assert( (1::2::3::Nil).tail == List(2,3), "the tail of an n-element list is the remaining n-1 elements")
+
+
   assert(           List() ==       Nil, "the empty list is nil")
   assert(     List(3).head ==         3, "the head of a single-element list it the element")
   assert(     List(3).tail ==       Nil, "the tail of a single-element list is Nil")
@@ -12,6 +19,11 @@ object InbuiltList {
     case Nil => 0
     case x :: xs => x + sum(xs)
   }
+
+  assert( sum(                  Nil ) ==  0, "sum of empty list should be 0")
+  assert( sum(             (3::Nil) ) ==  3, "sum of single-element list should be the element" )
+  assert( sum( (1::2::3::4::5::Nil) ) == 15, "sum of list should be sum of its elements")
+
 
   assert( sum(                 Nil ) ==  0, "sum of empty list should be 0")
   assert( sum(             List(3) ) ==  3, "sum of single-element list should be the element" )
